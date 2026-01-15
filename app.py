ea4eab51a53650c81f4d3f68acf0916102606d48
manojnehra1801-beep <manojnehra1801@gmail.com>
2026-01-15 21:05:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def start_mock_1():
     session["q_index"] = 0
     session["answers"] = {}
 
-    return render_template("ssc_cgl_mock_1.html")
+    return render_template("ssc_cgl_exam_1.html")
 
 
 # ================= PAYMENT =================
